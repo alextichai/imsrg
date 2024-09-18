eda51dc48bb566ffae69ee16b9c89674fb9fdef4
Alexander Tichai <a_tichai@theorie.ikp.physik.tu-darmstadt.de>
2024-09-18 13:03:10 +0200

    adjusted submission script to loop over isotopic chains more easily

--- work/scripts/goUniversal.py
+++ work/scripts/goUniversal.py
@@ -25,7 +25,7 @@ elif call('type '+'srun', shell=True, stdout=PIPE, stderr=PIPE) == 0: BATCHSYS =
 ### The code uses OpenMP and benefits from up to at least 24 threads
 NTHREADS=24
 exe = '/Users/alexandertichai/Work/Code/imsrg++/src/build/imsrg++'
-exe = '/home/tichai/code/imsrg/src/build_preset/intel/imsrg++'
+# exe = '/home/tichai/code/imsrg/src/build_preset/intel/imsrg++'
 
 ### Flag to swith between submitting to the scheduler or running in the current shell
 batch_mode=False
@@ -56,7 +56,7 @@ ARGS['omega_norm_max'] = '0.25'
 
 ### Model space parameters used for reading Darmstadt-style interaction files
 ARGS['file2e1max'] = '12 file2e2max=24 file2lmax=12'
-ARGS['file3e1max'] = '14 file3e2max=28 file3e3max=14'
+ARGS['file3e1max'] = '18 file3e2max=36 file3e3max=24'
 
 ### Name of a directory to write Omega operators so they don't need to be stored in memory. If not given, they'll just be stored in memory.
 #ARGS['scratch'] = 'SCRATCH'    
@@ -112,87 +112,79 @@ time srun %s
 ### Make a directory for the log files, if it doesn't already exist
 if not path.exists('imsrg_log'): mkdir('imsrg_log')
 
-### Loop over multiple jobs to submit
-for Z in range(28,29):
- A=78
- for reference in ['%s%d'%(ELEM[Z],A)]:
-  ARGS['reference'] = reference
-  print('Z = ', Z)
-  for e in [6,8,10,12]:
-   for hw in [16]:
+# Loop over multiple jobs to submit
+for Z in range(10,11):
+  for N in range(8,22,2):
+    A = N + Z 
+    reference = '%s%d'%(ELEM[Z],A)
+  # for reference in ['%s%d'%(ELEM[Z],A)]:
+    ARGS['reference'] = reference
+    print(reference)
 
-     ARGS['emax'] = '%d'%e
-     ARGS['e3max'] = '24'
+    for e in [6]:
+      for hw in [16]:
+        ARGS['emax'] = '%d'%e
+        ARGS['e3max'] = '24'
+ 
+#     ARGS['2bme'] = '/data_share11/takayuki/me2j/TwBME-HO_NN-only_N3LO_EM500_srg1.8_hw%i_emax18_e2max36.me2j.gz'%(hw)
+#     ARGS['3bme'] = '/data_share11/takayuki/me3j/NO2B_ThBME_EM1.8_2.0_3NFJmax15_IS_hw%i_ms18_36_24.stream.bin'%(hw)    
 
-     #ARGS['2bme'] = '/Users/alexandertichai/Work/Matrixelements/ME2J/chi2b_srg0800_eMax12_hwHO020.me2j.gz'
-     #ARGS['3bme'] = 'input/me3j/chi2b3b400cD-02cE0098_hwconv036_srg0625ho40J_eMax14_EMax14_hwHO0%d.me3j.gz'%(hw)
-     #ARGS['LECs'] = 'srg0625'
-     #ARGS['3bme'] = ''
-
-     ARGS['2bme'] = '/data_share11/takayuki/me2j/TwBME-HO_NN-only_N3LO_EM500_srg1.8_hw%i_emax18_e2max36.me2j.gz'%(hw)
-     ARGS['3bme'] = '/data_share11/takayuki/me3j/NO2B_ThBME_EM1.8_2.0_3NFJmax15_IS_hw%i_ms18_36_24.stream.bin'%(hw)    
-     ARGS['LECs'] = ''
-     ARGS['3bme_type'] = 'no2b'
-
-
-     ARGS['LECs'] = ''
+        ARGS['2bme'] = '/Users/alexandertichai/Work/Matrixelements/ME2J/TwBME-HO_NN-only_N3LO_EM500_srg1.8_hw%i_emax14_e2max28.me2j.gz'%(hw)
+        ARGS['3bme'] = '/Users/alexandertichai/Work/Matrixelements/ME3J/NO2B_ThBME_EM1.8_2.0_3NFJmax15_JJmax15_IS_hw%i_ms18_36_24.stream.bin'%(hw)    
+    #  ARGS['3bme'] = 'none'
+        ARGS['LECs'] = 'EM_1.8_2.0'
+        ARGS['3bme_type'] = 'no2b'
 
 
-     ARGS['hw'] = '%d'%hw
-     ARGS['A'] = '%d'%A
-     ARGS['valence_space'] = reference
-#     ARGS['valence_space'] = '0hw-shell'
-#     ARGS['valence_space'] = 'Cr%d'%A
-#     ARGS['core_generator'] = 'imaginary-time'
-#     ARGS['valence_generator'] = 'shell-model-imaginary-time'
-     ARGS['emax'] = '%d'%e
-#     ARGS['method'] = method
+        ARGS['hw'] = '%d'%hw
+        ARGS['A'] = '%d'%A
+        ARGS['valence_space'] = reference
 
-    #  ARGS['Operators'] = ''    # Operators to consistenly transform, separated by commas.
-#     ARGS['Operators'] = 'Rp2'
+        ARGS['emax'] = '%d'%e
+
+        # ARGS['Operators'] = ''    # Operators to consistenly transform, separated by commas.
+        ARGS['Operators'] = 'Rp2'
     #  ARGS['Operators'] = 'Rp2,Rn2,RdotR'
-     ARGS['Operators'] = 'Rm2lab'
+        # ARGS['Operators'] = 'Rm2lab'
 #     ARGS['Operators'] = 'E2'
 #     ARGS['Operators'] = 'E2,M1'
 #     ARGS['Operators'] = 'E2,M1,GamowTeller'
 #     ARGS['Operators'] = 'M1p,M1n,Sigma_p,Sigma_n'
 #     ARGS['Operators'] = 'GamowTeller'
 
+        ### Make an estimate of how much time to request. Only used for slurm at the moment.
+        time_request = '24:00:00'
+        if   e <  5 : time_request = '00:10:00'
+        elif e <  8 : time_request = '01:00:00'
+        elif e < 10 : time_request = '04:00:00'
+        elif e < 12 : time_request = '12:00:00'
 
+        jobname  = '%s_%s_%s_%s_e%s_E%s_s%s_hw%s_A%s' %(ARGS['valence_space'], ARGS['LECs'],ARGS['method'],ARGS['reference'],ARGS['emax'],ARGS['e3max'],ARGS['smax'],ARGS['hw'],ARGS['A'])
+        logname = jobname + datetime.fromtimestamp(time()).strftime('_%y%m%d%H%M.log')
 
-    ### Make an estimate of how much time to request. Only used for slurm at the moment.
-     time_request = '24:00:00'
-     if   e <  5 : time_request = '00:10:00'
-     elif e <  8 : time_request = '01:00:00'
-     elif e < 10 : time_request = '04:00:00'
-     elif e < 12 : time_request = '12:00:00'
+        ### Some optional parameters that we probably want in the output name if we're using them
+        if 'lmax3' in ARGS:  jobname  += '_l%d'%(ARGS['lmax3'])
+        if 'eta_criterion' in ARGS: jobname += '_eta%s'%(ARGS['eta_criterion'])
+        if 'core_generator' in ARGS: jobname += '_' + ARGS['core_generator']
+        if 'BetaCM' in ARGS: jobname += '_' + ARGS['BetaCM']
+        ARGS['flowfile'] = 'output/BCH_' + jobname + '.dat'
+        ARGS['intfile']  = '/Users/alexandertichai/Work/Projects/IMSRG/' + jobname
 
-     jobname  = '%s_%s_%s_%s_e%s_E%s_s%s_hw%s_A%s' %(ARGS['valence_space'], ARGS['LECs'],ARGS['method'],ARGS['reference'],ARGS['emax'],ARGS['e3max'],ARGS['smax'],ARGS['hw'],ARGS['A'])
-     logname = jobname + datetime.fromtimestamp(time()).strftime('_%y%m%d%H%M.log')
+        cmd = ' '.join([exe] + ['%s=%s'%(x,ARGS[x]) for x in ARGS])
 
-  ### Some optional parameters that we probably want in the output name if we're using them
-     if 'lmax3' in ARGS:  jobname  += '_l%d'%(ARGS['lmax3'])
-     if 'eta_criterion' in ARGS: jobname += '_eta%s'%(ARGS['eta_criterion'])
-     if 'core_generator' in ARGS: jobname += '_' + ARGS['core_generator']
-     if 'BetaCM' in ARGS: jobname += '_' + ARGS['BetaCM']
-     ARGS['flowfile'] = 'output/BCH_' + jobname + '.dat'
-     ARGS['intfile']  = '/Users/alexandertichai/Work/Projects/IMSRG/' + jobname
-
-     cmd = ' '.join([exe] + ['%s=%s'%(x,ARGS[x]) for x in ARGS])
-
-  ### Submit the job if we're running in batch mode, otherwise just run in the current shell
-     if batch_mode==True:
-       sfile = open(jobname+'.batch','w')
-       if BATCHSYS == 'PBS':
-         sfile.write(FILECONTENT%(jobname,environ['PWD'],NTHREADS,mail_address,logname,NTHREADS,cmd))
-         sfile.close()
-         call(['qsub', jobname+'.batch'])
-       elif BATCHSYS == 'SLURM':
-         sfile.write(FILECONTENT%(NTHREADS,jobname,time_request,mail_address,NTHREADS,NTHREADS,cmd))
-         sfile.close()
-         call(['sbatch', jobname+'.batch'])
-       remove(jobname+'.batch') # delete the file
-       sleep(0.1)
-     else:
-       call(cmd.split())  # Run in the terminal, rather than submitting
+        ### Submit the job if we're running in batch mode, otherwise just run in the current shell
+        if batch_mode==True:
+          sfile = open(jobname+'.batch','w')
+          if BATCHSYS == 'PBS':
+            sfile.write(FILECONTENT%(jobname,environ['PWD'],NTHREADS,mail_address,logname,NTHREADS,cmd))
+            sfile.close()
+            call(['qsub', jobname+'.batch'])
+          elif BATCHSYS == 'SLURM':
+            sfile.write(FILECONTENT%(NTHREADS,jobname,time_request,mail_address,NTHREADS,NTHREADS,cmd))
+            sfile.close()
+            call(['sbatch', jobname+'.batch'])
+          remove(jobname+'.batch') # delete the file
+          sleep(0.1)
+        else:
+          call(cmd.split())  # Run in the terminal, rather than submitting
 
